c0dc730f29d85bd87bc6f475899279a1e5864988
montazeri_a <montazeri_a@office.zharfpouyan.net>
2022-06-08 17:54:29 +0430

    dropdown && modal

--- js/src/footman.js
+++ js/src/footman.js
@@ -1,10 +1,17 @@
 "use strict";
 document.addEventListener('click', function (e) {
-    console.log(e.target.classList);
-    e.target.classList.forEach(function (element) {
+    var _a;
+    var target = e.target;
+    (_a = target === null || target === void 0 ? void 0 : target.classList) === null || _a === void 0 ? void 0 : _a.forEach(function (element) {
         switch (element) {
             case 'modalBtn':
-                new Modal();
+                Modal.btnModal();
+                break;
+            case 'close':
+                Modal.closeModal();
+                break;
+            case 'f-dropdown-content':
+                new DropDown();
                 break;
             case 'f-dropdown-btn':
                 new DropDown();
@@ -14,34 +21,29 @@ document.addEventListener('click', function (e) {
 });
 var Modal = /** @class */ (function () {
     function Modal() {
-        Modal.modalBtn.addEventListener('click', function () {
-            Modal.modal.style.display = 'block';
-        });
-        Modal.modalClose.addEventListener('click', function () {
-            Modal.modal.style.display = 'none';
-        });
     }
+    Modal.btnModal = function () {
+        Modal.modal.style.display = 'block';
+    };
+    Modal.closeModal = function () {
+        Modal.modal.style.display = 'none';
+    };
     /**
      * زماینکه از سلکتور اسم کلاس استفاده میشود
      * حتما باید اندیس اول را انتخاب کنید وگرنه خطا میدهد
      */
-    Modal.modalBtn = document.getElementsByClassName('modalBtn')[0];
     Modal.modal = document.getElementsByClassName('modal')[0];
-    Modal.modalClose = document.getElementsByClassName('close')[0];
     return Modal;
 }());
 var DropDown = /** @class */ (function () {
     function DropDown() {
-        DropDown.dropdownBtn.addEventListener('click', function () {
-            if (DropDown.dropdownCtn.style.display === '') {
-                DropDown.dropdownCtn.style.display = 'block';
-            }
-            else {
-                DropDown.dropdownCtn.style.display = '';
-            }
-        });
+        if (DropDown.dropdownCtn.style.display === '') {
+            DropDown.dropdownCtn.style.display = 'block';
+        }
+        else {
+            DropDown.dropdownCtn.style.display = '';
+        }
     }
-    DropDown.dropdownBtn = document.getElementsByClassName('f-dropdown-btn')[0];
     DropDown.dropdownCtn = document.getElementsByClassName("f-dropdown-content")[0];
     return DropDown;
 }());
--- js/src/footman.ts
+++ js/src/footman.ts
@@ -1,48 +1,50 @@
 document.addEventListener('click', (e) => {
-    console.log(e.target.classList);
-    e.target.classList.forEach(element => {
+    const target = e.target as HTMLTextAreaElement;
+    target?.classList?.forEach((element: any) => {
         switch(element) {
             case 'modalBtn':
-                new Modal();
+                Modal.btnModal();
+            break;
+
+            case 'close':
+                Modal.closeModal();
+            break;
+
+            case 'f-dropdown-content':
+                new DropDown();
             break;
 
             case 'f-dropdown-btn':
                 new DropDown();
             break;
+        }
     });
 });
 
-class Modal{
+class Modal {
     /**
      * زماینکه از سلکتور اسم کلاس استفاده میشود
      * حتما باید اندیس اول را انتخاب کنید وگرنه خطا میدهد
      */
-    private static modalBtn: (any) = document.getElementsByClassName('modalBtn')[0];
     private static modal: (any) = document.getElementsByClassName('modal')[0];
-    private static modalClose: (any) = document.getElementsByClassName('close')[0];
 
-    constructor() {
-        Modal.modalBtn.addEventListener('click', () => {
-            Modal.modal.style.display = 'block';
-        });
-        
-        Modal.modalClose.addEventListener('click', () => {
-            Modal.modal.style.display = 'none';
-        });
+    public static btnModal() {
+        Modal.modal.style.display = 'block';
+    }
+
+    public static closeModal() {
+        Modal.modal.style.display = 'none';
     }
 }
 
 class DropDown{
-    private static dropdownBtn: (any) = document.getElementsByClassName('f-dropdown-btn')[0];
     private static dropdownCtn: (any) = document.getElementsByClassName("f-dropdown-content")[0];
 
     constructor() {
-        DropDown.dropdownBtn.addEventListener('click', () => {
-            if(DropDown.dropdownCtn.style.display === '') {
-                DropDown.dropdownCtn.style.display = 'block';
-            } else {
-                DropDown.dropdownCtn.style.display = '';
-            }
-        })
+        if(DropDown.dropdownCtn.style.display === '') {
+            DropDown.dropdownCtn.style.display = 'block';
+        } else {
+            DropDown.dropdownCtn.style.display = '';
+        }
     }
 }
